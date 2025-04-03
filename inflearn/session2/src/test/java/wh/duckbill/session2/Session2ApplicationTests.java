d035604141a566273100eb662171d9a8c62c7eab
duckbill <uhyeon7399@daum.net>
2025-04-04 02:03:19 +0900

    initialize session2

--- inflearn/session2/src/test/java/wh/duckbill/session2/Session2ApplicationTests.java
+++ inflearn/session2/src/test/java/wh/duckbill/session2/Session2ApplicationTests.java
@@ -1,10 +1,10 @@
-package wh.duckbill.inflearn;
+package wh.duckbill.session2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InflearnApplicationTests {
+class Session2ApplicationTests {
 
   @Test
   void contextLoads() {
