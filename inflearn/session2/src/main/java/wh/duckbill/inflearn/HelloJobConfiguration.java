a0ab32fa6192f6f82d73e892f40199f23ad93f2c
duckbill <uhyeon7399@daum.net>
2025-04-07 00:12:49 +0900

    DB 스키마 생성 및 이해 (1)

--- inflearn/session2/src/main/java/wh/duckbill/inflearn/HelloJobConfiguration.java
+++ inflearn/session2/src/main/java/wh/duckbill/inflearn/HelloJobConfiguration.java
@@ -16,17 +16,31 @@ public class HelloJobConfiguration {
   private final StepBuilderFactory stepBuilderFactory;
 
   @Bean
-  public Job helloJob(){
+  public Job helloJob() {
     return jobBuilderFactory.get("helloJob")
-        .start(this.helloStep())
+        .start(this.helloStep1())
+        .next(this.helloStep2())
         .build();
   }
 
   @Bean
-  public Step helloStep(){
-    return stepBuilderFactory.get("helloStep")
+  public Step helloStep1() {
+    return stepBuilderFactory.get("helloStep1")
         .tasklet((contribution, chunkContext) -> {
-          System.out.println("hello spring batch");
+          System.out.println("==================================");
+          System.out.println("hello spring batch 1");
+          System.out.println("==================================");
+          return RepeatStatus.FINISHED;
+        }).build();
+  }
+
+  @Bean
+  public Step helloStep2() {
+    return stepBuilderFactory.get("helloStep2")
+        .tasklet((stepContribution, chunkContext) -> {
+          System.out.println("==================================");
+          System.out.println("hello spring batch 2");
+          System.out.println("==================================");
           return RepeatStatus.FINISHED;
         }).build();
   }
