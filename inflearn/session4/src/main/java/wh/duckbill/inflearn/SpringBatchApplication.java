e386a5969861c4610300902336851e9bf33008d9
duckbill413 <uhyeon7399@daum.net>
2025-04-07 22:16:00 +0900

    DB 스키마 생성 및 이해 (1)

---
+++ inflearn/session4/src/main/java/wh/duckbill/inflearn/SpringBatchApplication.java
@@ -0,0 +1,20 @@
+package wh.duckbill.inflearn;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @EnableBatchProcessing
+ * 총 4개의 설정 클래스를 실행시키며 스프링 배치의 모든 초기화 및 실행 구성이 이루어짐
+ * 스프링 부트 배치의 자동 설정 클래스가 실행됨으로 빈으로 등록된 모든 Job 을 검색하여 초기화와 동시에 Job 을 수행하도록 구성됨
+ */
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBatchApplication.class, args);
+  }
+
+}
